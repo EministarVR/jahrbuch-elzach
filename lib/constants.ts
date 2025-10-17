12c61fae40f275aad063cc9ccf819bd1f3a010fe
emin <benounnaelemin@gmail.com>
2025-10-17 12:16:32 +0200

    update

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,7 @@
 // Central constants for the Jahrbuch app
 // Adjust this list to match the actual school classes
 export const CLASSES: string[] = [
-  'W5a', 'W6a','W6b','W7a','W7b','W8a','W8b', 'W9a', 'W10', 'R5a','R5b','R6a','R6b', 'R6c', 'R7a','R7b', 'R8a', 'R8b', 'R8c', 'R9a', 'R9b', 'R9c', 'R10a', 'R10b', 'HVKL'
+  'W5a', 'W6a','W6b','W7a','W7b','W8a','W8b', 'W9a', 'W10', 'R5a','R5b','R6a','R6b', 'R6c', 'R7a','R7b', 'R8a', 'R8b', 'R8c', 'R9a', 'R9b', 'R9c', 'R10a', 'R10b', 'HVKL', 'Lehrer'
 ];
 
 export const CATEGORIES = [
@@ -9,9 +9,6 @@ export const CATEGORIES = [
     'Klassen',
     'Lehrer',
     'AGs',
-    'Sport',
-    'Musik',
-    'Kunst',
     'Events',
 ] as const;
 
